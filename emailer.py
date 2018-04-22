9b4e78773af83602a60405a2e965180f0d96f1c5
carloscalderon <cdcalderon@gmail.com>
2018-04-22 02:34:06 -0500

    add try except block around  email file open code

--- emailer.py
+++ emailer.py
@@ -1,7 +1,11 @@
 emails = []
-email_file = open('emailsdoc.txt', 'r')
 
-for line in email_file:
-  emails.append(line)
+try:
+  email_file = open('emailsdoc.txt', 'r')
+
+  for line in email_file:
+    emails.append(line.strip())
+except FileNotFoundError as err:
+  print(err)  
 
 print(emails)
