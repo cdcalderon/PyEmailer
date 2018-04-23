3c17ef86f3b1f76ec95196e862f8b9061aaf9282
carloscalderon <cdcalderon@gmail.com>
2018-04-23 01:08:15 -0500

    move code to functions get_emails, get_schedule

--- emailer.py
+++ emailer.py
@@ -1,21 +1,27 @@
-emails = {}
+def get_emails():
+  emails = {}
 
-try:
-  email_file = open('emailsdoc.txt', 'r')
+  try:
+    email_file = open('emailsdoc.txt', 'r')
 
-  for line in email_file:
-    (email, username) = line.split(',')
-    emails[email] = username.strip()
-except FileNotFoundError as err:
-  print(err)  
+    for line in email_file:
+      (email, username) = line.split(',')
+      emails[email] = username.strip()
+  except FileNotFoundError as err:
+    print(err)  
 
-print(emails)
+  return emails 
 
-try:
-  schedule_file = open('schedule.txt', 'r')
-  schedule = schedule_file.read()
+def get_schedule():
+  try:
+    schedule_file = open('schedule.txt', 'r')
+    schedule = schedule_file.read()
 
-  print(schedule)
-except FileNotFoundError as err:
-  print(err)
+    print(schedule)
+  except FileNotFoundError as err:
+    return err
+
+## test functions
+get_emails()
+get_schedule()
 
